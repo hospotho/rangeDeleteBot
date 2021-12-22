7a0590efa20610628742534d97824ab4cc784a02
hospotho <a155268747@gmail.com>
2021-12-22 18:20:52 +0800

    add show logs

--- index.js
+++ index.js
@@ -35,43 +35,63 @@ const discord_js_1 = __importStar(require("discord.js"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const prefix = '!!';
+var debug = false;
 const client = new discord_js_1.default.Client({
     intents: [discord_js_1.Intents.FLAGS.GUILDS, discord_js_1.Intents.FLAGS.GUILD_MESSAGES]
 });
+class logStack {
+    constructor(_size = 50) {
+        this.logs = [];
+        this.maxsize = _size;
+    }
+    logging(str) {
+        if (this.logs.length === this.maxsize)
+            this.logs.shift();
+        this.logs.push(str);
+        console.log(str);
+    }
+    getLog(size) {
+        return this.logs.slice(Math.max(this.logs.length - size, 0));
+    }
+    clear() {
+        this.logs = [];
+    }
+}
+var logger = new logStack();
 client.on('ready', () => {
-    console.log('The bot is ready.');
+    logger.logging('The bot is ready.');
 });
 function msToMinSec(ms) {
     let min = Math.floor(ms / 60000);
     let sec = Math.floor((ms % 60000) / 1000);
     return (min > 0 ? min + 'm' : '') + (sec < 10 && min > 0 ? '0' : '') + sec + 's';
 }
-function rangedelete(message) {
+function rangedelete(msg) {
     return __awaiter(this, void 0, void 0, function* () {
-        if (!message.member || message.channel.type == 'DM')
-            return;
+        const message = msg;
+        const channel = message.channel;
         const args = message.content.split(' ');
         if (args[1] === args[2]) {
-            message.channel.send(`MessageID(1) and MessageID(2) should not be the same.`);
+            channel.send(`MessageID(1) and MessageID(2) should not be the same.`);
             return;
         }
         if (parseInt(args[1]) > parseInt(args[2])) {
-            message.channel.send(`Message(1) is newer than Message(2).`);
+            channel.send(`Message(1) is newer than Message(2).`);
             return;
         }
-        function fetch(id, channel = message.channel) {
+        function fetch(_id, _ch = channel) {
             return __awaiter(this, void 0, void 0, function* () {
                 try {
-                    const msg = yield channel.messages.fetch(id);
+                    const msg = yield _ch.messages.fetch(_id);
                     return msg;
                 }
                 catch (error) {
-                    const channels = channel.guild.channels.cache;
+                    const channels = _ch.guild.channels.cache;
                     for (const [_, ch] of channels) {
                         if (!ch.isText())
                             continue;
                         try {
-                            const msg = yield ch.messages.fetch(id);
+                            const msg = yield ch.messages.fetch(_id);
                             return msg;
                         }
                         catch (error) {
@@ -83,25 +103,26 @@ function rangedelete(message) {
         }
         const msg1 = yield fetch(args[1]);
         if (!msg1) {
-            message.channel.send(`Message(1) ${args[1]} not found.`);
+            channel.send(`Message(1) ${args[1]} not found.`);
             return;
         }
         const msg2 = yield fetch(args[2]);
         if (!msg2) {
-            message.channel.send(`Message(2) ${args[2]} not found.`);
+            channel.send(`Message(2) ${args[2]} not found.`);
             return;
         }
-        if (msg1.channel != msg2.channel) {
-            message.channel.send(`Messages need to be in same channel.`);
+        if (channel != msg1.channel || msg1.channel != msg2.channel) {
+            channel.send(`Messages need to be in same channel.`);
             return;
         }
         let startTime = Date.now();
-        let botMsg = yield message.channel.send(`Starting to delete messages from ${args[1]} to ${args[2]}.`).then(sent => {
+        yield message.delete();
+        let botMsg = yield channel.send(`Starting to delete messages from ${args[1]} to ${args[2]}.`).then(sent => {
             return sent;
         });
-        console.log(`range delete start by ${message.author.username} at #${message.channel.name} id: ${message.id}`);
-        yield message.channel.send(`<:gbf_makira_gun:685481376400932895>`);
-        let msgs = yield msg1.channel.messages.fetch({
+        logger.logging(`Range delete start by ${message.author.username} at #${channel.name} id: ${message.id}`);
+        yield channel.send(`<:gbf_makira_gun:685481376400932895>`);
+        let msgs = yield channel.messages.fetch({
             after: msg1.id,
             limit: 99
         });
@@ -109,41 +130,39 @@ function rangedelete(message) {
         msgs = msgs.sort((a, b) => a.createdTimestamp - b.createdTimestamp);
         yield msg1.delete();
         let count = 1;
-        yield message.channel.bulkDelete(msgs).then(msg => (count += msg.size));
+        yield channel.bulkDelete(msgs).then(msg => (count += msg.size));
         while (!msgs.has(msg2.id)) {
-            yield botMsg.edit(`Still deleting, ${count} messages deleted so far`).then(() => console.log(`${count} messages deleted`));
+            yield botMsg.edit(`Still deleting, ${count} messages deleted so far`).then(() => logger.logging(`${count} messages deleted`));
             let tmp = msgs.lastKey();
-            msgs = yield msg1.channel.messages.fetch({
+            msgs = yield channel.messages.fetch({
                 after: tmp,
                 limit: 100
             });
             msgs = msgs.filter(m => m.createdTimestamp <= msg2.createdTimestamp);
             msgs = msgs.sort((a, b) => a.createdTimestamp - b.createdTimestamp);
-            yield message.channel.bulkDelete(msgs).then(msg => (count += msg.size));
+            yield channel.bulkDelete(msgs).then(msg => (count += msg.size));
         }
         let timeCost = msToMinSec(Date.now() - startTime);
         yield botMsg.edit(`Complete, ${count} messages deleted in ${timeCost}`).then(() => {
-            console.log(`Complete, ${count} messages deleted in ${timeCost} id: ${message.id}`);
+            logger.logging(`Complete, ${count} messages deleted in ${timeCost} id: ${message.id}`);
         });
     });
 }
 client.on('messageCreate', message => {
     if (!message.content.startsWith(prefix))
         return;
+    if (!message.member || !message.guild)
+        return;
+    // check permissions
+    if (message.author.id !== message.guild.ownerId) {
+        if (!message.member.permissionsIn(message.channel).has(discord_js_1.default.Permissions.FLAGS.MANAGE_MESSAGES) ||
+            message.author.bot) {
+            logger.logging(`permission denied, range delete start by ${message.author.username} at #${message.channel.name} id: ${message.id}`);
+            return;
+        }
+    }
     let args = message.content.split(' ');
     if (args[0].slice(2) === 'rangedelete') {
-        if (!message.member)
-            return;
-        if (!message.guild)
-            return;
-        // check permissions
-        if (message.author.id !== message.guild.ownerId) {
-            if (!message.member.permissionsIn(message.channel).has(discord_js_1.default.Permissions.FLAGS.MANAGE_MESSAGES) ||
-                message.author.bot) {
-                console.log('permission denied');
-                return;
-            }
-        }
         if (args.length != 3) {
             message.channel.send({
                 content: 'Invalid arguments count\nUsage:  !!rangedelete  MessageID1  MessageID2'
@@ -152,6 +171,27 @@ client.on('messageCreate', message => {
         }
         rangedelete(message);
         return;
+    }
+    if (args[0].slice(2) === 'logs') {
+        let size = 20;
+        if (args.length == 2) {
+            if (isNaN(parseInt(args[1], 10))) {
+                message.channel.send({
+                    content: 'Invalid arguments count\nUsage:  !!logs  (size)'
+                });
+                return;
+            }
+            size = parseInt(args[1], 10);
+        }
+        let log = '`' + logger.getLog(size).reduce((a, b) => a + '\n' + b) + '`';
+        message.channel.send({
+            content: log
+        });
+    }
+    if (args[0].slice(2) === 'help') {
+        message.channel.send({
+            content: '**`command list:`**`\n!!rangedelete  MessageID1  MessageID2\n!!logs (Size)\n!!help`'
+        });
     }
 });
 client.login(process.env.TOKEN);
--- index.ts
+++ index.ts
@@ -2,12 +2,35 @@ import DiscordJS, {Intents, Message, TextChannel, Permissions} from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
 const prefix = '!!'
+var debug = false
 const client = new DiscordJS.Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 })
 
+class logStack {
+  logs: Array<string>
+  maxsize: number
+  constructor(_size: number = 50) {
+    this.logs = []
+    this.maxsize = _size
+  }
+  logging(str: string) {
+    if (this.logs.length === this.maxsize) this.logs.shift()
+    this.logs.push(str)
+    console.log(str)
+  }
+  getLog(size: number) {
+    return this.logs.slice(Math.max(this.logs.length - size, 0))
+  }
+  clear() {
+    this.logs = []
+  }
+}
+
+var logger = new logStack()
+
 client.on('ready', () => {
-  console.log('The bot is ready.')
+  logger.logging('The bot is ready.')
 })
 
 function msToMinSec(ms: number) {
@@ -16,28 +39,30 @@ function msToMinSec(ms: number) {
   return (min > 0 ? min + 'm' : '') + (sec < 10 && min > 0 ? '0' : '') + sec + 's'
 }
 
-async function rangedelete(message: Message) {
-  if (!message.member || message.channel.type == 'DM') return
+async function rangedelete(msg: Message) {
+  const message = msg
+  const channel = message.channel as TextChannel
+
   const args = message.content.split(' ')
   if (args[1] === args[2]) {
-    message.channel.send(`MessageID(1) and MessageID(2) should not be the same.`)
+    channel.send(`MessageID(1) and MessageID(2) should not be the same.`)
     return
   }
   if (parseInt(args[1]) > parseInt(args[2])) {
-    message.channel.send(`Message(1) is newer than Message(2).`)
+    channel.send(`Message(1) is newer than Message(2).`)
     return
   }
 
-  async function fetch(id: string, channel: TextChannel = message.channel as TextChannel): Promise<Message | undefined> {
+  async function fetch(_id: string, _ch: TextChannel = channel): Promise<Message | undefined> {
     try {
-      const msg = await channel.messages.fetch(id)
+      const msg = await _ch.messages.fetch(_id)
       return msg
     } catch (error) {
-      const channels = channel.guild.channels.cache
+      const channels = _ch.guild.channels.cache
       for (const [_, ch] of channels) {
         if (!ch.isText()) continue
         try {
-          const msg = await ch.messages.fetch(id)
+          const msg = await ch.messages.fetch(_id)
           return msg
         } catch (error) {
           continue
@@ -48,27 +73,29 @@ async function rangedelete(message: Message) {
 
   const msg1 = await fetch(args[1])
   if (!msg1) {
-    message.channel.send(`Message(1) ${args[1]} not found.`)
+    channel.send(`Message(1) ${args[1]} not found.`)
     return
   }
   const msg2 = await fetch(args[2])
   if (!msg2) {
-    message.channel.send(`Message(2) ${args[2]} not found.`)
+    channel.send(`Message(2) ${args[2]} not found.`)
     return
   }
 
-  if (msg1.channel != msg2.channel) {
-    message.channel.send(`Messages need to be in same channel.`)
+  if (channel != msg1.channel || msg1.channel != msg2.channel) {
+    channel.send(`Messages need to be in same channel.`)
     return
   }
 
   let startTime = Date.now()
-  let botMsg = await message.channel.send(`Starting to delete messages from ${args[1]} to ${args[2]}.`).then(sent => {
+  await message.delete()
+  let botMsg = await channel.send(`Starting to delete messages from ${args[1]} to ${args[2]}.`).then(sent => {
     return sent
   })
-  console.log(`range delete start by ${message.author.username} at #${message.channel.name} id: ${message.id}`)
-  await message.channel.send(`<:gbf_makira_gun:685481376400932895>`)
-  let msgs = await msg1.channel.messages.fetch({
+  logger.logging(`Range delete start by ${message.author.username} at #${channel.name} id: ${message.id}`)
+  await channel.send(`<:gbf_makira_gun:685481376400932895>`)
+
+  let msgs = await channel.messages.fetch({
     after: msg1.id,
     limit: 99
   })
@@ -76,44 +103,46 @@ async function rangedelete(message: Message) {
   msgs = msgs.sort((a, b) => a.createdTimestamp - b.createdTimestamp)
   await msg1.delete()
   let count = 1
-  await message.channel.bulkDelete(msgs).then(msg => (count += msg.size))
+  await channel.bulkDelete(msgs).then(msg => (count += msg.size))
 
   while (!msgs.has(msg2.id)) {
-    await botMsg.edit(`Still deleting, ${count} messages deleted so far`).then(() => console.log(`${count} messages deleted`))
+    await botMsg.edit(`Still deleting, ${count} messages deleted so far`).then(() => logger.logging(`${count} messages deleted`))
     let tmp = msgs.lastKey()
-    msgs = await msg1.channel.messages.fetch({
+    msgs = await channel.messages.fetch({
       after: tmp,
       limit: 100
     })
     msgs = msgs.filter(m => m.createdTimestamp <= msg2.createdTimestamp)
     msgs = msgs.sort((a, b) => a.createdTimestamp - b.createdTimestamp)
-    await message.channel.bulkDelete(msgs).then(msg => (count += msg.size))
+    await channel.bulkDelete(msgs).then(msg => (count += msg.size))
   }
 
   let timeCost = msToMinSec(Date.now() - startTime)
   await botMsg.edit(`Complete, ${count} messages deleted in ${timeCost}`).then(() => {
-    console.log(`Complete, ${count} messages deleted in ${timeCost} id: ${message.id}`)
+    logger.logging(`Complete, ${count} messages deleted in ${timeCost} id: ${message.id}`)
   })
 }
 
 client.on('messageCreate', message => {
   if (!message.content.startsWith(prefix)) return
 
+  if (!message.member || !message.guild) return
+  // check permissions
+  if (message.author.id !== message.guild.ownerId) {
+    if (
+      !message.member.permissionsIn(message.channel as TextChannel).has(DiscordJS.Permissions.FLAGS.MANAGE_MESSAGES) ||
+      message.author.bot
+    ) {
+      logger.logging(
+        `permission denied, range delete start by ${message.author.username} at #${(message.channel as TextChannel).name} id: ${message.id}`
+      )
+      return
+    }
+  }
+
   let args = message.content.split(' ')
 
   if (args[0].slice(2) === 'rangedelete') {
-    if (!message.member) return
-    if (!message.guild) return
-    // check permissions
-    if (message.author.id !== message.guild.ownerId) {
-      if (
-        !message.member.permissionsIn(message.channel as TextChannel).has(DiscordJS.Permissions.FLAGS.MANAGE_MESSAGES) ||
-        message.author.bot
-      ) {
-        console.log('permission denied')
-        return
-      }
-    }
     if (args.length != 3) {
       message.channel.send({
         content: 'Invalid arguments count\nUsage:  !!rangedelete  MessageID1  MessageID2'
@@ -122,6 +151,29 @@ client.on('messageCreate', message => {
     }
     rangedelete(message)
     return
+  }
+
+  if (args[0].slice(2) === 'logs') {
+    let size = 20
+    if (args.length == 2) {
+      if (isNaN(parseInt(args[1], 10))) {
+        message.channel.send({
+          content: 'Invalid arguments count\nUsage:  !!logs  (size)'
+        })
+        return
+      }
+      size = parseInt(args[1], 10)
+    }
+    let log = '`' + logger.getLog(size).reduce((a, b) => a + '\n' + b) + '`'
+    message.channel.send({
+      content: log
+    })
+  }
+
+  if (args[0].slice(2) === 'help') {
+    message.channel.send({
+      content: '**`command list:`**`\n!!rangedelete  MessageID1  MessageID2\n!!logs (Size)\n!!help`'
+    })
   }
 })
 
